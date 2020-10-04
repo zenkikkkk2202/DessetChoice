689f2792b5010d8f1c8e69cacd1f20b2c9f8863b
zenkikkkk2202 <57906987+zenkikkkk2202@users.noreply.github.com>
2020-10-04 16:56:52 +0900

    create intent_function

--- app/src/main/java/com/example/masaki/dessetchoice/MainActivity.kt
+++ app/src/main/java/com/example/masaki/dessetchoice/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.example.masaki.dessetchoice
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MotionEvent
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+    }
+
+    override fun onTouchEvent(event: MotionEvent?): Boolean {
+        val intent = Intent(this,ListActivity::class.java)
+        startActivity(intent)
+
+        return true
     }
 }
